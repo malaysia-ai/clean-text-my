2a15b9cea5b70b37a2d0ba20d9ce45128a5f71b9
amzar96 <mohdamzar96@gmail.com>
2023-08-25 18:32:50 +0800

    fix: update version & init

--- clean_text_my/__init__.py
+++ clean_text_my/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from . import deduplication
 from . import postprocessing
 from . import download_dataset
 from . import utils
 from . import logging
+from . import manipulation
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 setup(
     name=__packagename__,
     description="",
-    version="0.1.0",
+    version="0.1.1",
     long_description=readme(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
